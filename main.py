0ef4f0820ec2a16aba0acd600885c6b641dfee51
phyushin <phyushin@gmail.com>
2025-02-21 00:20:08 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from PyPDF2 import PdfWriter as Writer, PdfReader as Reader
+from PyPDF2.generic import AnnotationBuilder
+import argparse
+
+__maj__ = 1
+__min__ = 0
+__rev__ = 0
+
+__author__ = "Phyu"
+
+
+__version__ = f"{__maj__}.{__min__}.{__rev__}"
+page_box = [
+        0,
+        0,
+        612,
+        792
+    ]
+
+output = Writer()
+
+def banner():
+    if not args.quiet:
+        print(f"""
+              
+░  ░░░░░░░░  ░░░░  ░░░      ░░░░      ░░░░░░░░░   ░░░  ░░░      ░░░        ░░        ░
+▒  ▒▒▒▒▒▒▒▒   ▒▒   ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒▒▒▒▒▒▒    ▒▒  ▒▒  ▒▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒▒▒
+▓  ▓▓▓▓▓▓▓▓        ▓▓  ▓▓▓▓  ▓▓  ▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓  ▓  ▓▓  ▓▓▓▓  ▓▓▓▓▓  ▓▓▓▓▓      ▓▓▓
+█  ████████  █  █  ██        ██  ████  ████████  ██    ██        █████  █████  ███████
+█        ██  ████  ██  ████  ███      █████████  ███   ██  ████  █████  █████        █
+                                                                                     
+                                                                               
+Version: {__version__}
+Author: {__author__}
+              """)
+
+
+
+parser = argparse.ArgumentParser(
+    prog='lmaonate',
+    description='A python script to add whole page links into PDFs rather than have to pay for acrobat writer'
+)
+
+parser.add_argument("-i","--infile",dest="infile", required=True)
+parser.add_argument("-o","--outfile",dest="outfile", required=True)
+parser.add_argument("-u","--url",dest="url", required=True)
+parser.add_argument("-q",dest="quiet", action="store_true")
+
+args = parser.parse_args() 
+
+
+
+def readPDF(file_path: str):
+    with open (file_path, "rb") as f:
+        pdf = Reader(f)
+        page_count = len(pdf.pages)
+        
+        for i in range(page_count):
+            p = pdf.pages[i]
+            output.add_page(p)
+
+
+            
+            url = args.url
+
+            link = AnnotationBuilder.link(page_box, None, url=url)
+            #output.addLink(i, url, rect)
+          
+            output.add_annotation(page_number=i,annotation= link)
+
+            with open(args.outfile,"wb") as out_stream:
+                output.write(out_stream)
+
+
+
+def main():
+    banner()
+    readPDF(args.infile)
+
+if __name__ == "__main__":
+    main()
